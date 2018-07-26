1ca29fc91dc4e0f2384c21d7571d8e69c7328b07
Szymon <sz.sniadach@gmail.com>
2018-07-26 23:55:31 +0200

    - create section "holy masses" - style button

--- src/app/application/controllers/Main.php
+++ src/app/application/controllers/Main.php
@@ -12,6 +12,7 @@ class Main extends CI_Controller {
 
 		$this->load->view('pages/main/hero');
 		$this->load->view('pages/main/announcements');
+		$this->load->view('pages/main/holy-masses');
 		
 		$this->load->view('partials/footer');
 	}
---
+++ src/app/application/views/pages/main/holy-masses.php
@@ -0,0 +1,29 @@
+<section class="holy-masses p-3">
+
+    <div class="container">
+        <h2 class="section-heading-white">porządek mszy świętej:</h2>
+
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <p>Dni powszednie:</p>
+                <span class="paragraph">- godz. 6.30, 7.00, 18.00.</span>
+                <br><br>
+
+                <p>W niedzielę i święta:</p>
+                <span class="paragraph">- godz. 6.30, 8.00 - w kościele dolnym</span><br>
+                <span class="paragraph">- godz. 9.30, 11.00, 12.30, 14.00, 18.00 - w kościele górnym</span>
+            </div>
+            <div class="col-12 col-md-6">
+                <div class="text-center">
+                    <img src="img/kielich.png" alt="kielich">
+                </div>
+            </div>
+        </div>
+
+        <div class="text-center">
+            <a class="btn" href="#">Więcej informacji</a>
+        </div>
+        
+    </div>
+
+</section>
